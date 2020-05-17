cb72bceba173452b3ae63f479523ba5df6122325
CoolPix0 <62768656+CoolPix0@users.noreply.github.com>
2020-05-17 09:46:55 +0200

    Update Account.java

--- banking/Account.java
+++ banking/Account.java
@@ -2,15 +2,26 @@ package banking;
 
 public class Account {
 
-  private double balance;
+  private double   balance;
 
   public Account(double bal) {
     balance = bal;
   }
-public double getBalance() {
+
+  public double getBalance() {
     return balance;
   }
   public boolean deposit(double amount) {
     balance = balance + amount;
     return true;
+  }
+  public boolean withdraw(double amount) {
+    boolean result = true;
+    if ( balance < amount ) {
+      result = false;
+    } else {
+      balance = balance - amount;
+    }
+    return result;
+  }
 }
