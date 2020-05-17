6db6f549fd1b56a4fe6d116af52468f182293e41
Czaroslavov <62669823+Czaros@users.noreply.github.com>
2020-05-17 13:14:55 +0200

    Update TestBanking.java

--- TestBanking.java
+++ TestBanking.java
@@ -10,3 +10,12 @@ public class TestBanking {
     System.out.println("Creating the customer Jane Smith.");
     customer = new Customer("Jane", "Smith");
     
+    System.out.println("Creating her account with a 500.00 balance.");
+    customer.setAccount(new Account(500.00));
+    account = customer.getAccount();
+    
+    // Perform some account transactions
+    System.out.println("Withdraw 150.00: " + account.withdraw(150.00));
+    System.out.println("Deposit 22.50: " + account.deposit(22.50));
+    System.out.println("Withdraw 47.62: " + account.withdraw(47.62));
+    System.out.println("Withdraw 400.00: " + account.withdraw(400.00));
